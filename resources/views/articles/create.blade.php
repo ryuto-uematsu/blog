76efff41dfd4675ffbaf30f6aa2b81025ce23755
ryuto <n44n7zb3mvangjzykv1m@docomo.ne.jp>
2023-10-24 08:12:58 +0000

    レイアウトの作成

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,34 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="/main.css">
-</head>
-<body>
-    <header>
-        <div class="site-title">ミニブログ</div>
-    </header>
-    <main class="container">
-        {{--作成した記事の内容を送信する--}}
-        <form action="{{ route('articles.store') }}" method="post">
-            {{--csrfディレクティブはform要素内に記述しておくとCSRF攻撃を防ぐことができる--}}
-            @csrf 
-            <dl class="form-list">
-                <dt>タイトル</dt>
-                <dd><input type="text" name="title"></dd>
-                <dt>本文</dt>
-                <dd><textarea name="body" rows="5"></textarea></dd>
-            </dl>
-            <button type="submit">投稿する</button>
-            {{--記事一覧に戻るリンク。ルート名で指定している。--}}
-            <a href="{{ route('articles.index') }}">キャンセル</a>
-        </form>
-    </main>
-    <footer>
-        &copy; Laravel8 入門から開発実践まで
-    </footer>
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+<form action="{{ route('articles.store') }}" method="post">
+@csrf 
+<dl class="form-list">
+    <dt>タイトル</dt>
+    <dd><input type="text" name="title"></dd>
+    <dt>本文</dt>
+    <dd><textarea name="body" rows="5"></textarea></dd>
+</dl>
+<button type="submit">投稿する</button>
+<a href="{{ route('articles.index') }}">キャンセル</a>
+</form>
+@endsection()
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,31 +1,10 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="/main.css">
-</head>
-<body>
-    <header>
-        <div class="site-title"><span>ミニブログ</span></div>
-    </header>
-    <main class="container">
-        {{--記事作成へのリンク--}}
-         <p><a href="{{ route('articles.create') }}">記事を書く</a></p>
-        {{--変数は変える↓--}}
-        @foreach ($articles as $article)
-        <article class="article-item">
-            {{--モデルからタイトルを取得--}}
-            <div class="article-title">{{ $article->title }}</div>
-            {{--モデルから本文を取得--}}
-            <div class="article-body">{{ $article->body }}</div>
-        </article>
-        @endforeach
-    </main>
-    <footer>
-        ミニブログ
-    </footer>
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+<p><a href="{{ route('articles.create') }}">記事を書く</a></p>
+@foreach ($articles as $article)
+<article class="article-item">
+    <div class="article-title">{{ $article->title }}</div>
+    <div class="article-body">{{ $article->body }}</div>
+</article>
+@endforeach
+@endsection()
